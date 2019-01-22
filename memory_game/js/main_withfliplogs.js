f4e5f651d5d90fcd34f636d2c72ede1147acfc0e
Jeremy Tsukasa Zimmerman <jtzimmerm@gmail.com>
2019-01-21 23:00:55 -0500

    created game logic

---
+++ memory_game/js/main_withfliplogs.js
@@ -0,0 +1,8 @@
+console.log("User flipped" + cardOne);
+console.log("User flipped" + cardThree);
+
+var cards = ["queen" , "queen" , "king" , "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push('cardOne');
+
